e6efd1e247eed644021924ea21e2618bf91071e0
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2025-03-20 14:24:05 +0100

    initial commit

---
+++ src/main/java/HelloFX.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        GridPane gridpane = new GridPane();
+
+        System.out.println(System.getProperty("user.dir"));
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                ImageView imgView = new ImageView(new Image(
+                        "file:/mnt/c/Users/HdP/Documents/github/javaFx/src/main/resources/img" + (i * 2 + j) + ".jpg"));
+                imgView.setFitWidth(300);
+                imgView.setFitHeight(300);
+                gridpane.add(imgView, i, j);
+            }
+        }
+
+        Scene scene = new Scene(gridpane, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
